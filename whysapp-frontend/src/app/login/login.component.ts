1f0bd7ec8a0b49a58e98aadbf61dfab8ec25359d
Will Mear <williammear@yahoo.com>
2023-03-16 16:44:11 +0000

    login page

--- whysapp-frontend/src/app/login/login.component.ts
+++ whysapp-frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private userServices: UserService) {}
+  
+  ngOnInit(): void {
+
+  }
+
+  
+
 }
+
