c00a2f1d1c7e16993afbf0231d5bccb9f2ab47b6
Will Mear <williammear@yahoo.com>
2023-03-20 22:19:45 +0000

    role specific changes

--- user/src/main/java/com/whysapp/app/security/auth/AuthenticationController.java
+++ user/src/main/java/com/whysapp/app/security/auth/AuthenticationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 @RequiredArgsConstructor
 public class AuthenticationController {
 
